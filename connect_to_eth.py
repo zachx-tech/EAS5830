a2442af8f00d8be1a169810d6137e9f115b415fe
zachx-tech <zachx@seas.upenn.edu>
2025-10-20 04:09:10 +0800

    Refactor connect_with_middleware for readability

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -19,28 +19,26 @@ def connect_to_eth():
 
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# TODO complete this method
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	bsc_url = "https://bsc-dataseed.binance.org/"
+    bsc_url = "https://bsc-dataseed.binance.org/"  # BSC mainnet public RPC
 
-	w3 = Web3(HTTPProvider(bsc_url))
-	assert w3.is_connected(), f"Failed to connect to BSC provider at {bsc_url}"
+    
+    w3 = Web3(HTTPProvider(bsc_url))
+    assert w3.is_connected(), f"Failed to connect to BSC provider at {bsc_url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-	
-  checksum_address = Web3.to_checksum_address(address)
-  contract = w3.eth.contract(address=checksum_address, abi=abi)
+    
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+    
 
-	return w3, contract
+    checksum_address = Web3.to_checksum_address(address)
+    contract = w3.eth.contract(address=checksum_address, abi=abi)
+
+    return w3, contract
 
 
 if __name__ == "__main__":
